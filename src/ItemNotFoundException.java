38bd7c7574e0e0b3eef0cac28e41fb56c985087e
amundas7 <abhimundas46@gmail.com>
2025-12-24 16:47:28 -0600

    Added comments to explain the exception

--- src/ItemNotFoundException.java
+++ src/ItemNotFoundException.java
@@ -1,3 +1,8 @@
+/*
+This class creates a custom exception for when an item is not found by extending the Exception class
+It has a constructor that passes the message to the parent class, which stores it
+Author: Abhi Mundas
+ */
 public class ItemNotFoundException extends Exception{
     public ItemNotFoundException(String message) {
         super(message);
